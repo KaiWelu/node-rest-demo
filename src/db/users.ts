90d42f79004151a3bb2ec2fd95e2b0a2419de711
KaiWelu <kaiweluda@gmail.com>
2024-09-06 17:13:21 +0200

    adds crud for the users

--- src/db/users.ts
+++ src/db/users.ts
@@ -14,5 +14,32 @@ const UserSchema = new mongoose.Schema({
 });
 
 // this makes the schema available with the name "User"
-
 export const UserModel = mongoose.model("User", UserSchema);
+
+// this gets all users by running find with no arguments
+export const getUsers = () => UserModel.find();
+
+// this finds one user by the specified email
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+
+//
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    "authentication.sessionToken": sessionToken,
+  });
+
+//
+export const getUserById = (id: string) => UserModel.findById(id);
+
+// creates user with the given values and converts it to object
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject);
+
+// deletes user by given id, mongoose gives everything an _id for convinience
+export const deleteUserById = (id: string) => {
+  UserModel.findOneAndDelete({ _id: id });
+};
+
+//
+export const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate({ id, values });
