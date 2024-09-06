ef05e5d7b168baea829b5e9f108f28f2105b53f1
KaiWelu <kaiweluda@gmail.com>
2024-09-06 17:33:43 +0200

    adds authentication methods and a controller for registering users

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { createUser, getUserByEmail } from "db/users";
+import { authentication, random } from "helpers";
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    // checks if any fields are missing
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    // checks if user exists
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    // this is how you respond with a status
+    return res.sendStatus(400);
+  }
+};
---
+++ src/helpers/index.ts
@@ -0,0 +1,12 @@
+import crypto from "crypto";
+const SECRET = "NODE-REST-API";
+
+// this is not safe because i don't understand everything, also the SECRET shouldnt be here
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
