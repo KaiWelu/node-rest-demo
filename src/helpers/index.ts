d87c0029aeff2c42550a0a6a1de1dafe7a726a14
KaiWelu <kaiweluda@gmail.com>
2024-09-06 17:52:31 +0200

    fixes bug in module paths

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,6 +2,7 @@ import crypto from "crypto";
 const SECRET = "NODE-REST-API";
 
 // this is not safe because i don't understand everything, also the SECRET shouldnt be here
+// but it's fine for demo purposes
 
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string, password: string) => {
