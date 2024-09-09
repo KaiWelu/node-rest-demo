c24329208e6dcf06426264cf23c771a13c226850
KaiWelu <kaiweluda@gmail.com>
2024-09-09 12:50:19 +0200

    adds login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -4,6 +4,60 @@ import express from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { authentication, random } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    // extract expected fields from request
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      console.log("400 - password or email is missing");
+      return res.sendStatus(400);
+    }
+
+    //check for an existing user
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      console.log("400 - no user found");
+      return res.sendStatus(400);
+    }
+
+    // authenticate user without knowing their password by using hash comparison
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    // check the hash of the request with the one in the database for the user
+    if (user.authentication.password !== expectedHash) {
+      console.log("403 - access forbidden");
+      return res.sendStatus(403);
+    }
+
+    // creates a user session token and updates the user model object
+    // see https://mongoosejs.com/docs/documents.html for reference
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+    await user.save();
+
+    // this  will set a cookie with the session token
+    res.cookie("NODE-DEMO", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    // return status and user and end the stream
+    console.log("200 - successful login");
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    console.log("400 - bad login request");
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
